126369bc3dd269b84ef2b7761ab45bed9e268683
Vyacheslav Ivanov <slavaiv2003@gmail.com>
2024-12-04 23:45:37 +0700

    Delete blogicum

--- blogicum/pages/tests.py
+++
@@ -1,3 +0,0 @@
-from django.test import TestCase  # type: ignore[import-untyped] # noqa: F401
-
-# Create your tests here.
--- blogicum/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path  # type: ignore[import-untyped]
-from . import views
-
-app_name = 'pages'
-
-urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('rules/', views.rules, name='rules'),
-]
