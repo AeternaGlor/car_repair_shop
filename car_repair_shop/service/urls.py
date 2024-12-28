108948d8a9c04f4d2f8890912dc355d8600d8730
Vyacheslav <snego.bro@gmail.com>
2024-12-28 18:31:49 +0300

    views and templates

--- car_repair_shop/service/urls.py
+++ car_repair_shop/service/urls.py
@@ -4,11 +4,20 @@ from . import views
 app_name = 'service'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('services/<slug:service_name>/',
-         views.service_detail,
-         name='service_detail'),
-    path('category/<slug:master_slug>/',
-         views.master_detail,
-         name='master_detail'),
+     path('', views.index, name='index'),
+     path('services/<slug:service_slug>/',
+          views.service_detail,
+          name='service_detail'
+     ),
+     path('orders/>',
+          views.order_list,
+          name='order_list'
+     ),
+     path('orders/<int:order_id/>',
+          views.order_detail,
+          name='order_detail'
+     ),
+     path('category/<slug:master_slug>/',
+          views.master_detail,
+          name='master_detail'),
 ]
--- car_repair_shop/service/views.py
+++ car_repair_shop/service/views.py
@@ -1,85 +1,34 @@
-from django.shortcuts import render  # type: ignore[import-untyped]
+from datetime import timezone
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from slugify import slugify
-
-
-services = [
-    {
-        'id': 0,
-        'name': 'Техническое обслуживание',
-        'master': 'Иванов В.А.',
-        'date': '16.10.2022',
-        'text': '''Замена масла и масляного фильтра.
-                   Проверка и замена воздушного, топливного и салонного
-                   фильтров. Проверка состояния свечей зажигания и их
-                   замена при необходимости. Проверка уровня и состояния всех
-                   жидкостей (тормозная жидкость, антифриз, жидкость
-                   гидроусилителя руля и т.д.).''',
-    },
-    {
-        'id': 1,
-        'name': 'Диагностика',
-        'master': 'Степанов В.А.',
-        'date': '16.10.2022',
-        'text': '''Компьютерная диагностика для выявления неисправностей
-                   в электронных системах автомобиля.Проверка состояния
-                   двигателя, трансмиссии, подвески и других ключевых систем.
-                   ''',
-    },
-    {
-        'id': 2,
-        'name': 'Ремонт двигателя',
-        'master': 'Василенко В.И.',
-        'date': '16.10.2022',
-        'text': '''Ремонт и замена компонентов двигателя, таких как поршни,
-                   кольца, клапаны и т.д. Ремонт или замена головки блока
-                   цилиндров. Ремонт или замена турбокомпрессора.''',
-    },
-    {
-        'id': 3,
-        'name': 'Ремонт трансмиссии',
-        'master': 'Сурвилов А.О.',
-        'date': '16.10.2022',
-        'text': '''Ремонт и замена компонентов коробки передач
-                   (механической или автоматической). Ремонт сцепления.
-                   Ремонт или замена карданного вала и других элементов
-                   трансмиссии.''',
-    },
-    {
-        'id': 4,
-        'name': 'Ремонт тормозной системы',
-        'master': 'Павлов А.А.',
-        'date': '16.10.2022',
-        'text': '''Замена тормозных колодок и дисков.
-                   Проверка и замена тормозных шлангов и трубок.
-                   Ремонт и замена главного тормозного цилиндра и
-                   других компонентов тормозной системы.''',
-    },
-]
-
-for service in services:
-    service['master_slug'] = slugify(service['master'])
-    service['name_slug'] = slugify(service['name'])
-
-new_services = {service['name_slug']: service for service in services}
 
 
 # Create your views here.
 def index(request):
     template_name = 'service/index.html'
+
+    services = Service.objects.select_related(
+        'master'
+    ).filter()
+
     context = {
-        'services': services.__reversed__()
+        'services': services
     }
     return render(request, template_name, context)
 
 
-def service_detail(request, service_name):
+def service_detail(request, service_slug):
     template_name = 'service/service_detail.html'
 
-    if str(service_name) not in new_services.keys():
-        raise Http404(f'Нет услуги с таким названием: {service_name}')
-    
-    service = new_services[str(service_name)]
+    service = get_object_or_404(
+        Service.objects.select_related(
+            'master'
+        ),
+    ).filter(
+        'service__is_shown' is True,
+        'master__is_shown' is True
+    )
 
     context = {
         'service': service
@@ -90,19 +39,54 @@ def service_detail(request, service_name):
 
 def master_detail(request, master_slug):
     template_name = 'service/master_detail.html'
+
+    master = get_object_or_404(Master, slug=master_slug)
+
     context = {
-        'master': 'Аноним'
+        'master': master
     }
-    for service in new_services.values():
-        if service['master_slug'] == master_slug:
-            context['master'] = service['master']
-            break
-    
+
     return render(request, template_name, context)
 
 
-if __name__ == "__main__":
-    # new_services = {service['master_slug'] = slugify(service['master']) for name, service in new_services.items()}
-    for name, service in new_services.items():
-        service['master_slug'] = 'slaggggg'
-    print(new_services['Ремонт тормозной системы'])
+def order_list(request):
+    template_name = 'service/order_list.html'
+
+    order_list = Order.objects.select_related(
+        'service',
+        'master'
+    ).filter(
+        'service__is_shown' is True,
+        'master__is_shown' is True,
+        'start_time' == timezone.now()
+    )
+
+    context = {
+        'order_list': order_list
+    }
+
+    return render(request, template_name, context)
+
+
+def order_detail(request, order_id):
+    template_name = 'service/order_detail.html'
+
+    order = get_object_or_404(
+        Order.objects.select_related(
+            'service',
+            'box'
+        ),
+        order_id
+    )
+
+    customer = get_object_or_404(Customer, order=order)
+
+    total_price = order.service.price * customer.discount / 100
+
+    context = {
+        'order': order,
+        'customer': customer,
+        'total_price': total_price
+    }
+
+    return render(request, template_name, context)
