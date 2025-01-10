7098745d5a905ae1ce1f14ca35a8643ea885fd04
Vyacheslav Ivanov <slavaiv2003@gmail.com>
2025-01-10 21:32:11 +0800

    Add user ligin, logout

--- car_repair_shop/car_repair_shop/urls.py
+++ car_repair_shop/car_repair_shop/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('', include('service.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
